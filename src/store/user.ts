60b053de27d57e0c9b30b6d1b4bc6d75e51692a4
Mariane Felix <mariane.felix.dev@gmail.com>
2024-03-31 19:54:03 -0300

    feat: altera store de usuário

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,11 +1,19 @@
 import { makeAutoObservable } from 'mobx';
 import { User } from '../models/user';
+import { Artist } from '../models/artist';
+import { makePersistable } from 'mobx-persist-store';
 
 export class UserStore {
   data = {} as User | null;
+  topArtits = [] as Artist[] | null;
 
   constructor() {
     makeAutoObservable(this);
+    makePersistable(this, {
+      name: 'UserStore',
+      properties: ['data', 'topArtits'],
+      storage: window.localStorage,
+    });
   }
 
   setUserData(data: User | null) {
@@ -14,6 +22,14 @@ export class UserStore {
 
   getUserData() {
     return this.data;
+  }
+
+  setTopArtits(artists: Artist[]) {
+    this.topArtits = artists;
+  }
+
+  getTopArtits() {
+    return this.topArtits;
   }
 }
 

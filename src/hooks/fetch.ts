a932c9727b1a498ed02ea19fc7bc282b2e7c41cf
Mariane Felix <mariane.felix.dev@gmail.com>
2024-04-01 17:19:26 -0300

    feat: adiciona função de refresh token

--- src/hooks/fetch.ts
+++ src/hooks/fetch.ts
@@ -2,39 +2,48 @@ import { useCallback, useState } from 'react';
 import api from '../services/api';
 import { authStore } from '../store/authentication';
 import { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
+import { useAuth } from './auth';
 
 const useFetch = () => {
+  const { refreshToken } = useAuth();
   const [isLoading, setIsLoading] = useState(true);
 
-  const fetchData = useCallback(async <T>(url: string, options?: AxiosRequestConfig) => {
-    setIsLoading(true);
-    let response = null as AxiosResponse<T, unknown> | null;
-    let error = null as AxiosError | null;
+  const fetchData = useCallback(
+    async <T>(url: string, options?: AxiosRequestConfig) => {
+      setIsLoading(true);
+      let response = null as AxiosResponse<T, unknown> | null;
+      let error = null as AxiosError | null;
 
-    try {
-      api.interceptors.request.use(
-        (config) => {
-          const token = authStore.getToken();
+      try {
+        api.interceptors.request.use(
+          (config) => {
+            const token = authStore.getToken();
 
-          if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-          }
+            if (token) {
+              config.headers.Authorization = `Bearer ${token}`;
+            }
 
-          return config;
-        },
+            return config;
+          },
 
-        (error) => Promise.reject(error)
-      );
+          (error) => Promise.reject(error)
+        );
 
-      response = await api.get<T>(url, options);
-    } catch (err) {
-      error = err as AxiosError;
-    }
+        response = await api.get<T>(url, options);
+      } catch (err) {
+        error = err as AxiosError;
 
-    setIsLoading(false);
+        if (error.status === 401) {
+          refreshToken(authStore.getRefreshToken());
+        }
+      }
 
-    return { response, error };
-  }, []);
+      setIsLoading(false);
+
+      return { response, error };
+    },
+    [refreshToken]
+  );
 
   return { fetchData, isLoading };
 };
